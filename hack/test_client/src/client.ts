458d3df68ab5d635680f6b3f2cc63e58d52fe422
cploujoux <ch.ploujoux@gmail.com>
2025-02-14 17:24:16 -0800

    Fix qdrant embedding

--- hack/test_client/src/client.ts
+++ hack/test_client/src/client.ts
@@ -1,89 +1,25 @@
-import { getFunctions, logger } from "@beamlit/sdk";
-import { LocalToolkit } from "@beamlit/sdk/functions/local.js";
-import { Client as ModelContextProtocolClient } from "@modelcontextprotocol/sdk/client/index.js";
-import { WebSocketClientTransport } from "@beamlit/sdk/functions/transport/websocket.js";
-import { MCPClient, MCPToolkit } from "@beamlit/sdk";
-import { createClient } from "@hey-api/client-fetch";
-import yargs from "yargs";
-import { hideBin } from 'yargs/helpers'
+// @ts-ignore
+import WebSocket from "ws";
+// @ts-ignore
+global.WebSocket = WebSocket;
 
+import { logger, newClient } from "@beamlit/sdk";
+import { LocalToolkit } from "@beamlit/sdk/functions/local.js";
+import { description, name, payload, url } from "./config.js";
 
 const main = async () => {
-    const argv = yargs(hideBin(process.argv))
-    .option('name', {
-      type: 'string',
-      description: 'Name of the function'
-    })
-    .option('description', {
-      type: 'string',
-      description: 'Description of the function'
-    })
-    .option('url', {
-      type: 'string',
-      description: 'URL of the function'
-    })
-    .option('list-tools', {
-      type: 'boolean',
-      description: 'List all tools'
-    })
-    .option('call-tool', {
-      type: 'string',
-      description: 'Call a tool with the given name'
-    })
-    .option('call-tool-args', {
-      type: 'object',
-      description: 'Call a tool with the given name and arguments'
-    })
-    .help()
-    .parseSync()
-  
-  if (argv.listTools) {
-    const functions = await getFunctions({ localFunctions: [{ name: argv.name, description: argv.description, url: argv.url }] });
-    functions.forEach((f) => {
-      logger.info(f.name);
-    logger.info(f.description);
-    logger.debug(f);
-  });
-
-  } else {}
-//     const modelContextProtocolClient = new ModelContextProtocolClient(
-//         {
-//             name: argv.name,
-//             version: "1.0.0",
-//         },
-//         {
-//             capabilities: {
-//                 tools: {}
-//             }
-//         }
-//     );
-
-//     const transport = new WebSocketClientTransport(new URL(argv.url), {
-//         "x-beamlit-authorization": "",
-//         "x-beamlit-workspace": "",
-//     });
-//     await modelContextProtocolClient.connect(transport);
-//     const mcpClient = new MCPClient(modelContextProtocolClient);
-//     const client = createClient({
-//         baseUrl: argv.url,
-//         headers: {
-//             "x-beamlit-authorization": "",
-//             "x-beamlit-workspace": "",
-//         },
-//     });
-//     const toolkit = new LocalToolkit(client, argv.name, argv.url);
-//     const mcpToolkit = new MCPToolkit(mcpClient);
-//     await mcpToolkit.initialize();
-//     const functions = await toolkit.getTools();
-//     const tool = functions.find((f) => f.name === argv.callTool);
-//     if (!tool) {
-//       logger.error(`Tool with name ${argv.callTool} not found`);
-//       return;
-//     }
-//     const result = await tool.invoke(argv.callToolArgs);
-//     logger.info(result);
-//   }
+  const client = newClient();
+  const toolkit = new LocalToolkit(client, name, url);
+  await toolkit.initialize(name, description);
+  const functions = await toolkit.getTools();
+  console.log(functions.map((f) => f.name));
+  const tool = functions.find((f) => f.name === payload.name);
+  if (!tool) {
+    logger.error(`Tool with name ${payload.name} not found`);
+    return;
+  }
+  const result = await tool.invoke(payload.arguments);
+  logger.info(result);
 };
-
 
 main();
---
+++ hack/test_client/src/config.ts
@@ -0,0 +1,9 @@
+export const payload = {
+  name: "qdrant_find_memories",
+  arguments: {
+    query: "What is the weather in San Francisco?",
+  },
+};
+export const description = "Find memories in Qdrant";
+export const name = "qdrant";
+export const url = "http://localhost:1400";
