e8496b2b92c5f6eaefcea4e5ebef2936f1d1d5b4
cploujoux <ch.ploujoux@gmail.com>
2025-02-14 17:24:26 -0800

    Fix required and not required

--- servers/src/utils.ts
+++ servers/src/utils.ts
@@ -1,25 +1,25 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 function transformInZodSchema(properties: any): z.ZodRawShape {
-    const schema: z.ZodRawShape = {};
-    for (const key in properties) {
-        schema[key] = toZodType(properties[key].type, properties[key].properties);
-    }
-    return schema;
+	const schema: z.ZodRawShape = {};
+	for (const key in properties) {
+		const zodType = toZodType(properties[key].type, properties[key].properties);
+		schema[key] = properties[key].required ? zodType : zodType.optional();
+	}
+	return schema;
 }
-
 
 function toZodType(type: string, properties: any): z.ZodType<any> {
-    if (type === "string") {
-        return z.string();
-    }
-    if (type === "number") {
-        return z.number();
-    }
-    if (type === "boolean") {
-        return z.boolean();
-    }
-    return z.object(transformInZodSchema(properties));
+	if (type === 'string') {
+		return z.string();
+	}
+	if (type === 'number') {
+		return z.number();
+	}
+	if (type === 'boolean') {
+		return z.boolean();
+	}
+	return z.object(transformInZodSchema(properties));
 }
 
-export { transformInZodSchema, toZodType };
+export { toZodType, transformInZodSchema };
