1bd00bf98c676ea3bb4d5888c408e83af193bf98
Joffref <mjoffre@beamlit.com>
2025-02-17 23:51:02 -0800

    Keep beamlit repo
    Signed-off-by: Joffref <mjoffre@beamlit.com>

--- hack/test_client/src/config.aws-s3.ts
+++ hack/test_client/src/config.aws-s3.ts
@@ -5,14 +5,14 @@ export const payload: ((previousResult: Record<string, any>) => {
   () => ({
     name: "list_files",
     arguments: {
-      bucket: "cli-beamlit-test",
+      bucket: "cli-blaxel-test",
     },
   }),
   () => {
     return {
       name: "upload_file",
       arguments: {
-        bucket: "cli-beamlit-test",
+        bucket: "cli-blaxel-test",
         key: "test.txt",
         data: "Hello, world!",
       },
