910c5a1035874bda4d5be6d43963fc6430840c47
cploujoux <cploujoux@blaxel.ai>
2025-06-14 11:59:18 -0700

    Test PG

--- hack/test_client/src/configs/config.postgres.ts
+++ hack/test_client/src/configs/config.postgres.ts
@@ -1,7 +1,14 @@
 export const payload: ((previousResult: Record<string, any>) => {
   name: string;
   arguments: Record<string, any>;
-})[] = [];
+})[] = [
+  () => ({
+    name: "query",
+    arguments: {
+      sql: "SELECT * FROM \"mcp-test-table\"",
+    },
+  }),
+];
 
 export const description = "Postgres description";
 export const name = "postgres";
