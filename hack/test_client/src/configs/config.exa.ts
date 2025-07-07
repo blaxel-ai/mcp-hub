c17a27b597a575f5be459dae765052255cf301aa
cploujoux <cploujoux@blaxel.ai>
2025-07-06 22:14:13 -0400

    Fix exa

--- hack/test_client/src/configs/config.exa.ts
+++ hack/test_client/src/configs/config.exa.ts
@@ -1,7 +1,14 @@
 export const payload: ((previousResult: Record<string, any>) => {
   name: string;
   arguments: Record<string, any>;
-})[] = [];
+})[] = [
+  () => ({
+    name: "web_search_exa",
+    arguments: {
+      query: "What is the capital of France?",
+    },
+  }),
+];
 
 export const description = "Exa description";
 export const name = "exa";
