322947e7c6f8a4ce7ebb32e0a8aea587711a059f
Thomas Crochet <thomas@gmail.com>
2025-09-11 22:38:20 +0200

    Fix dependabot

--- hack/test_client/src/configs/config.gmail.ts
+++
@@ -1,8 +0,0 @@
-export const payload: ((previousResult: Record<string, any>) => {
-  name: string;
-  arguments: Record<string, any>;
-})[] = [];
-
-export const description = "Gmail description";
-export const name = "gmail";
-export const url = "http://localhost:1400";
